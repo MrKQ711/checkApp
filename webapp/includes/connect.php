a4ac7b0e3655e5cfea385e38eb14154678e53eb3
Thái <92800620+MrKQ711@users.noreply.github.com>
2022-11-26 14:05:10 +0700

    Update connect.php

--- webapp/includes/connect.php
+++ webapp/includes/connect.php
@@ -1,6 +1,7 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "mystore");
+$con = new mysqli("localhost", "root", "", "mystore");
 if(!$con) {
-       die("Connection failed: " . mysqli_connect_error()); 
+       //die("Connection failed: " . mysqli_connect_error()); 
+       die("Connection failed: " . mysqli_error($con));
 }
